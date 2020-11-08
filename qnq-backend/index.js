745dc13291d05463a2c30a52882b181729b1096f
Arijit Saha <arijitbiley@gmail.com>
2020-11-08 19:44:59 +0530

    Remove Deprecations

--- qnq-backend/index.js
+++ qnq-backend/index.js
@@ -12,9 +12,14 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //connect to the database
-mongoose.connect(process.env.DB, { useNewUrlParser: true })
-  .then(() => console.log(`Database connected successfully`))
-  .catch(err => console.log(err));
+mongoose.connect(
+    process.env.DB,
+    {
+      useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true,
+      useUnifiedTopology: true
+    }
+  ).then(() => console.log(`Database connected successfully`))
+   .catch(err => console.log(err));
 
 //since mongoose promise is depreciated, we overide it with node's promise
 mongoose.Promise = global.Promise;
